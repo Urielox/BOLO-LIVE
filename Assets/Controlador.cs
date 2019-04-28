05a4583c248d96c2af2eb106fd264a23ac96c6e7
Urielox <urielox2011@hotmail.com>
2019-04-28 11:10:40 -0500

    first commit

---
+++ Assets/Controlador.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+   public void CambiarEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+    public void CambiarEscena1(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+}
